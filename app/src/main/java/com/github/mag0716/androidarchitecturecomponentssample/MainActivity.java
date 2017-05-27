ff54f8641c1deaeed31e8c03aa07e1385a40de62
Masashi Kishimoto <mkishimoto0716@gmail.com>
2017-05-28 02:10:32 +0900

    OnLifecycleEvent で定義した処理が動作することを確認する

--- app/src/main/java/com/github/mag0716/androidarchitecturecomponentssample/MainActivity.java
+++ app/src/main/java/com/github/mag0716/androidarchitecturecomponentssample/MainActivity.java
@@ -1,13 +1,58 @@
 package com.github.mag0716.androidarchitecturecomponentssample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.OnLifecycleEvent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import hugo.weaving.DebugLog;
 
+public class MainActivity extends LifecycleActivity {
+
+    @DebugLog
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getLifecycle().addObserver(new TestObserver());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    static class TestObserver implements LifecycleObserver {
+
+        @DebugLog
+        @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        void onCreate() {
+        }
+
+        @DebugLog
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+        void onStart() {
+        }
+
+        @DebugLog
+        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        void onResume() {
+        }
+
+        @DebugLog
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        void onPause() {
+        }
+
+        @DebugLog
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+        void onStop() {
+        }
+
+        @DebugLog
+        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        void onDestroy() {
+        }
     }
 }
