9ee17b79df5434e1c2af8920ae71ceb55c78e964
Masashi Kishimoto <mkishimoto0716@gmail.com>
2018-03-18 00:25:52 +0900

    課題1

--- lifecycle/src/main/java/com/github/mag0716/arch/lifecyclesample/MainActivity.java
+++ lifecycle/src/main/java/com/github/mag0716/arch/lifecyclesample/MainActivity.java
@@ -4,11 +4,16 @@ import android.arch.lifecycle.LifecycleObserver;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import hugo.weaving.DebugLog;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     private LifecycleObserver lifecycleObserver = new LoggingObserver();
     private Intent serviceIntent;
@@ -17,27 +22,71 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate : " + getLifecycle().getCurrentState());
+
+        Observable.just(1000)
+                .observeOn(Schedulers.io())
+                .subscribe(delay -> {
+                    Thread.sleep(delay);
+                    Log.d(TAG, "delayed onCreate : " + getLifecycle().getCurrentState());
+                });
+
         setContentView(R.layout.activity_main);
         getLifecycle().addObserver(lifecycleObserver);
     }
 
+    @DebugLog
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onCreate : " + getLifecycle().getCurrentState());
+    }
+
+    @DebugLog
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart : " + getLifecycle().getCurrentState());
+    }
+
+    @DebugLog
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume : " + getLifecycle().getCurrentState());
 
         serviceIntent = new Intent(this, LoggingService.class);
         startService(serviceIntent);
     }
 
+    @DebugLog
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState : " + getLifecycle().getCurrentState());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState : " + getLifecycle().getCurrentState());
+    }
+
+    @DebugLog
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d(TAG, "onPause : " + getLifecycle().getCurrentState());
         stopService(serviceIntent);
     }
 
+    @DebugLog
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop : " + getLifecycle().getCurrentState());
+    }
+
+    @DebugLog
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "onDestroy : " + getLifecycle().getCurrentState());
         getLifecycle().removeObserver(lifecycleObserver);
     }
 
